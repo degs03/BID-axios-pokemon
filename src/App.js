4cb89bacd76740964c2618c8d89b9327a02ac44d
Dario Gimenez <gimenezdario1617@gmail.com>
2023-02-05 22:11:05 -0300

    Poke-api with React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
-
-function App() {
+const App = () => {
+  const [pokemon, setPokemon] = useState([]);
+  const buscarPokemon = () => {
+      axios.get("https://pokeapi.co/api/v2/pokemon")
+        .then(response => {
+          return response.data;
+        }).then(response => {
+          console.log(response.results);
+          setPokemon(response.results.map((item) => item.name));
+        }).catch(err => {
+          console.log(err);
+        });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={buscarPokemon} className='boton'>Fetch Pokemon</button>
+      <ul>
+        {
+          pokemon.map((item, idx) => <li key={idx}>{item}</li>)
+        }
+      </ul>
     </div>
   );
 }
 
 export default App;
+
